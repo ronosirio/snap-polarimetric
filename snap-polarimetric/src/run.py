b2c5c6b9a6cdb8a6bbf594b5d9573b4ca0bb89e6
nekhtiari <n.ekhtiary@gmail.com>
2019-07-16 15:16:56 +0200

    Relocate the files

--- snap-polarimetric/src/run.py
+++
@@ -1,7 +0,0 @@
-"""
-This module runs the main class in snap_polarimetry script.
-"""
-from snap_polarimetry import SNAPPolarimetry
-
-if __name__ == "__main__":
-    SNAPPolarimetry().run()
--- snap-polarimetric/tests/context.py
+++
@@ -1,9 +0,0 @@
-"""
-This module is used in test_snap_polarimetry script.
-"""
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../libs')))
-
-from src.snap_polarimetry import SNAPPolarimetry #pylint: disable=unused-import,wrong-import-position
