8230947fb3d7f0ef737c568eae518e08d81a8aa0
António P. P. Almeida <antonio.almeida@up42.com>
2019-08-21 09:56:37 +0200

    * Reorganizing the config.mk vars.

--- config.mk
+++ config.mk
@@ -5,8 +5,8 @@ MANIFEST_JSON := $(SRC)/UP42Manifest.json
 UP42_DOCKERFILE := $(SRC)/Dockerfile
 JOB_CONFIG := $(SRC)/params.json  
 DOCKER_TAG := snap-polarimetric
+DOCKER_RUN_OPTIONS := --mount type=bind,src=/tmp/e2e_snap_polarimetric/output,dst=/tmp/output --mount type=bind,src=/tmp/e2e_snap_polarimetric/input,dst=/tmp/input
 ## Extra images needed by the block image.
 LIBS_DIR := $(SRC)/libs
 ESA_SNAP_DOCKERFILE := $(LIBS_DIR)/Dockerfile-esa-snap
 UP42_SNAP_DOCKERFILE := $(LIBS_DIR)/Dockerfile-up42-snap
-DOCKER_RUN_OPTIONS := --mount type=bind,src=/tmp/e2e_snap_polarimetric/output,dst=/tmp/output --mount type=bind,src=/tmp/e2e_snap_polarimetric/input,dst=/tmp/input
