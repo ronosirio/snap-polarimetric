5c94b310362b749be75d5519864441dd56275193
Rodrigo Almeida <rodrigo.almeida@up42.com>
2019-10-02 19:34:38 +0200

    Add cleanup of test cache

--- blocks/snap-polarimetric/test.sh
+++ blocks/snap-polarimetric/test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+find . -name "__pycache__" -exec rm -rf {} +
+find . -name ".mypy_cache" -exec rm -rf {} +
+find . -name ".pytest_cache" -exec rm -rf {} +
+find . -name ".coverage" -exec rm -f {} +
 
 python -m pytest --pylint --pylint-rcfile=../../pylintrc --mypy --mypy-ignore-missing-imports --cov=src/
